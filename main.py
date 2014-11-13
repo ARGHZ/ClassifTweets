d63728136f50455cb8d170945697c06039736ec5
Juan David Carrillo López <juand.carrillo@yahoo.com>
2014-11-13 09:42:10 -0600

    Se agregó un request para obtener tweets y usarlos como muestreo real. Esto modificó las cláusulas para manejar excepciones utilizadas en la codificación aritmética

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-__author__ = 'Argvz'
+__author__ = 'Juan David Carrillo López'
 from ngram import NGram
+from TwitterAPI import TwitterAPI
 import numpy as np
 
 from codigoaritm import *
@@ -20,6 +21,18 @@ if __name__ == '__main__':
     muestreo = leerarchivo('resources/lexico.txt')
     lexico = [par.split(',') for par in muestreo]
 
+    consumer_key = 'kxfJjFCXjkRySLkW2aHGeAXxN'
+    consumer_secret = 'VKalY6au6029H5uqo63VHH1VWcYwaBmlJ36EPulYUBmThyvDUi'
+    access_key = '1576798795-MJcRA8Yu8nfgDWbIQjshgio6bOoBCBOGZbSOF06'
+    access_secret = 'jPVa8ELVIDT2StlNJvts6UmZASllsliVdvHg7VikT88ew'
+
+    api = TwitterAPI(consumer_key, consumer_secret, access_key, access_secret)
+    respuesta = api.request('search/tweets', {'q': 'yamecanse', 'count': '100', 'lang': 'es'})
+    muestreo = []
+    for item in respuesta.get_iterator():
+        texto = item['text'].encode('latin-1', 'ignore')
+        muestreo.append(texto.decode('latin-1'))
+
     try:
         inst = CodigoAritm(alfabeto, probabilidades)
     except SimbProbsError as e:
@@ -31,16 +44,21 @@ if __name__ == '__main__':
                     'chngda mdre', 'p  to', 'pv t05', 'stpid', 'stpdo', 'indiota', 'Piche',
                     'inche m dre', 'q nches haces!?', 'q pichs!?', 'ndjo', 'ndja', 'ch...',
                     'chxgx xx madxe')
-        resultados = []
+        mensajes = tuple(muestreo)
+
         for mensaje in mensajes:
             caracteres = NGram(mensaje.split(' '))
-            print('\nEntropía de \'{0}\': {1}'.format(mensaje, str(inst.entropiadelmensaje(mensaje))))
-            inst.precodmsj(mensaje+'~')
-            for palabrota in lexico:
-                minusculas = palabrota[0].lower()
-                query = caracteres.search(minusculas)
-                coincidencias = [match for match in query if match[1] > 0.22]
-                if len(coincidencias) > 0:
-                    print('\tBuscando >> {0}: {1}'.format(minusculas, coincidencias))
+            try:
+                print('\nEntropía de \'{0}\': {1}'.format(mensaje, str(inst.entropiadelmensaje(mensaje))))
+                inst.precodmsj(mensaje+'~')
+            except ExistSimbError as e:
+                print('{0} \t Ignorando mensaje'.format(e))
+            else:
+                for palabrota in lexico:
+                    minusculas = palabrota[0].lower()
+                    query = caracteres.search(minusculas)
+                    coincidencias = [match for match in query if match[1] > 0.29]
+                    if len(coincidencias) > 0:
+                        print('\tBuscando >> {0}: {1}'.format(minusculas, coincidencias[0]))
     finally:
         print('\nTerminando ejecución del programa')
