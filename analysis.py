fa27bd6807666444268851736c42bc4dadf6a56b
Juan Carrillo <juand.carrillo@yahoo.com>
2015-11-25 17:50:21 -0600

    Analysis
    Updating the algorithm for making results' boxplots

--- analysis.py
+++ analysis.py
@@ -20,7 +20,7 @@ def saveandjoin30iter(type_dataset):
                     pass
                 else:
                     for ith_iter in range(1, 31):
-                        csvfile_path = 'recursos/resultados/{}/{}_{}_{}_{}.csv'. \
+                        csvfile_path = 'recursos/resultados/{}_{}_{}_{}_{}.csv'. \
                             format(type_dataset, t_clf, t_imprv, clf_name, ith_iter)
                         print 'Renderizing: {}'.format(csvfile_path)
 
@@ -31,7 +31,7 @@ def saveandjoin30iter(type_dataset):
                         new_arr.append(statistics)
                     new_arr = np.array(new_arr)
 
-                    guardar_csv(new_arr, 'recursos/resultados/{}/{}_{}_{}.csv'.
+                    guardar_csv(new_arr, 'recursos/resultados/{}_{}_{}_{}.csv'.
                                 format(type_dataset, t_clf, t_imprv, clf_name))
 
 
@@ -47,27 +47,27 @@ def getalldata(data_type, charts=False, f_image='jpeg'):
                         }
     for t_clf in data_experiment.keys():
         if t_clf == 'binary':
-            col_names = ['accuracy', 'precision class 1', 'precision class 2', 'recall class 1', 'recall class 2',
-                         'f1-score class 1', 'f1-score class 2', 'support class 1', 'support class 2']
+            col_names = ['Accuracy', 'P-class 1', 'P-class 2', 'R-class 1', 'R-class 2',
+                         'F1-class 1', 'F1-class 2', 'support class 1', 'support class 2']
+            support_class = 2
         else:
-            col_names = ['accuracy', 'precision class 1', 'precision class 2', 'precision class 3', 'recall class 1',
-                         'recall class 2', 'recall class 3', 'f1-score class 1', 'f1-score class 2', 'f1-score class 3',
+            col_names = ['Accuracy', 'P-class 1', 'P-class 2', 'P-class 3', 'R-class 1',
+                         'R-class 2', 'R-class 3', 'F1-class 1', 'F1-class 2', 'F1-class 3',
                          'support class 1', 'support class 2', 'support class 3']
+            support_class = 3
         for t_imprv in data_experiment[t_clf].keys():
             for clf_name in data_experiment[t_clf][t_imprv].keys():
-                csvfile_path = 'recursos/resultados/{}/{}_{}_{}.csv'.format(data_type, t_clf, t_imprv, clf_name)
+                csvfile_path = 'recursos/resultados/{}_{}_{}_{}.csv'.format(data_type, t_clf, t_imprv, clf_name)
                 print 'Getting: {}'.format(csvfile_path)
 
                 results = contenido_csv(csvfile_path)
                 results = np.array(results, dtype='f')
                 if charts:
                     plt.subplot()
-                    plt.boxplot(results[:, :7])
-                    plt.title('{}_{}_{}'.format(t_clf, t_imprv, clf_name))
-                    plt.xticks(np.arange(1, 8), ('accuracy', 'precision\nclass 1', 'precision\nclass 2',
-                                                 'recall\nclass 1', 'recall\nclass 2', 'f1-score\nclass 1',
-                                                 'f1-score\nclass 2'))
-                    plt.savefig('recursos/charts/{}_{}_{}.{}'.format(t_clf, t_imprv, clf_name, f_image))
+                    plt.boxplot(results[:, :len(col_names) - support_class])
+                    plt.title('{}_{}_{}_{}'.format(t_clf, data_type, t_imprv, clf_name))
+                    plt.xticks(np.arange(1, len(col_names) - support_class + 1), col_names, rotation='vertical')
+                    plt.savefig('recursos/charts/{}_{}_{}_{}.{}'.format(data_type, t_clf, t_imprv, clf_name, f_image))
                     plt.close()
                 data_experiment[t_clf][t_imprv][clf_name] = pd.DataFrame(results, columns=col_names)
     return data_experiment
@@ -76,21 +76,29 @@ def getalldata(data_type, charts=False, f_image='jpeg'):
 def structuriseresults(type_dataset):
     organised_data = getalldata(type_dataset)
 
-    improvements = ('kfolds', 'hparamt', 'eensemble', 'ros')
-    for t_clf in organised_data.keys():
-        if t_clf == 'binary':
-            col_name = 'f1-score class 2'
+    new_arr = []
+    for ith_tclf in organised_data.keys():
+        if ith_tclf == 'binary':
+            col_name = ['Accuracy', 'P-class 2', 'R-class 2', 'F1-class 2']
         else:
-            col_name = 'f1-score class 3'
-        for ith_improve in range(len(improvements)):
-            classifiers_data = organised_data[t_clf][improvements[ith_improve]]
-            filtered = np.array([classifiers_data[clf_name][col_name] for clf_name in classifiers_data.keys()]).T
-            filtered = pd.DataFrame(filtered, columns=[clf_name for clf_name in classifiers_data.keys()])
-            #  print filtered
-            print '\n{} - {} report:\n{}'.format(t_clf, improvements[ith_improve], filtered.mean(0))
+            col_name = ['Accuracy', 'P-class 3', 'R-class 3', 'F1-class 3']
+        for ith_improve in organised_data[ith_tclf].keys():
+
+            for ith_clf in organised_data[ith_tclf][ith_improve].keys():
+                filtered = organised_data[ith_tclf][ith_improve][ith_clf]
+                filtered = filtered[col_name]
+                means = filtered.mean(0)
+
+                full_name = '{}_{}_{}_{}'.format(type_dataset, ith_tclf, ith_improve, ith_clf)
+                temp_arra = [full_name, ]
+                new_arr.append((full_name, means[0], means[1], means[2], means[3]))
+                temp_arra.extend(means)
+                new_arr.append(temp_arra)
+                #  print '\n{} report:\n{}'.format(full_name, means[:])
+    guardar_csv(new_arr, 'recursos/resultados/clf_metricas_{}.csv'.format(type_dataset))
     print 'fin'
 
 
 if __name__ == '__main__':
-    structuriseresults('ngrams')
+    structuriseresults('nongrams')
     #  saveandjoin30iter('ngrams')
